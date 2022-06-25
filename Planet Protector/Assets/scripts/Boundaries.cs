c01a8353c6337e56c41ba3d72f3ed22296b488ab
Kuro.Amanjot <rattanamanjotsingh@gmail.com>
2022-06-25 14:59:04 +0530

    no message

--- Planet Protector/Assets/scripts/Boundaries.cs
+++ Planet Protector/Assets/scripts/Boundaries.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class Boundaries : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera MainCamera;
     private Vector2 screenBounds;
     private float objectWidth;
     private float objectHeight;
 
+    // Use this for initialization
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
     }
 
+    // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 objectPosition = transform.position;
-        objectPosition.x = Mathf.Clamp(objectPosition.x, screenBounds.x + objectWidth, screenBounds.x * -1 - objectWidth);
-        objectPosition.y = Mathf.Clamp(objectPosition.y, screenBounds.y + objectHeight, screenBounds.y * -1 - objectHeight);
-        transform.position = objectPosition;
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
     }
 }
  
